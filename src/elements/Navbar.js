9350c87f359a5cf8573bdfa23e1b1ab960154762
KimSinghz <ksinghz@outlook.com>
2023-08-03 23:52:59 -0700

    added home page

--- src/elements/Navbar.js
+++ src/elements/Navbar.js
@@ -6,21 +6,23 @@ import Nav from "react-bootstrap/Nav";
 import { Container } from "reactstrap";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./footer.css";
 
 const Navbars = () => {
   return (
     <div>
       <Navbar expand="lg" className="footer">
         <Container className="footer">
-          <FaRecycle size={30} className="icon-green" />
-          <Navbar.Brand href="#home">Team E-Cycle</Navbar.Brand>
+          <img className="logo-img" src="./logo.png"></img>
+          <Navbar.Brand href="home">Edushare</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link to="home">Home</Nav.Link>
-              <Nav.Link to="find-resources">Find Resources</Nav.Link>
-              <Nav.Link tp="donate-resources">Donate Resources</Nav.Link>
-              <Nav.Link to="contact">Contact Us</Nav.Link>
+              <Nav.Link href="/home">Home</Nav.Link>
+              <Nav.Link href="/find-resources">Find Resources</Nav.Link>
+              <Nav.Link href="/donate-resources">Donate Resources</Nav.Link>
+              {/*               <Nav.Link to="contact">Contact Us</Nav.Link>
+               */}
             </Nav>
           </Navbar.Collapse>
         </Container>
--- src/pages/App.js
+++ src/pages/App.js
@@ -6,13 +6,39 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <header className="App-header">
-        <h3>Welcome to Edushare</h3>
-        <p>
-          Our goal is to provide children with increasing access to resources
-          around the globe
-        </p>
-      </header>
+      <div>
+        <img
+          src="../poor-children.jpg"
+          alt="children"
+          style={{ width: "100%", filter: "brightness(40%)" }}
+        />
+        <div className="image-text">
+          <h3>Sharing is Caring.</h3>
+          <p>
+            Help millions of children around the world by joining our platform
+            now!
+          </p>
+          <button className="home-button">
+            <a href="/donate-resources">Donate</a>
+          </button>
+        </div>
+      </div>
+
+      <div className="second-part">
+        <img src="child-hw.jpg"></img>
+        <div>
+          <h3>Who Are We?</h3>
+          <p>
+            We are Edushare, the world's leading education platform, built by
+            high-schoolers, for children around the globe. Born from a humble
+            position of a Quadrant Resource internship, our team put mind and
+            soul together to figure out a way to give back to the community for
+            all it has given us. Our solution? A platform to children around the
+            globe share educational resources. Join our platform now to
+            contribute to our mission and help those less fortunate than you.
+          </p>
+        </div>
+      </div>
       <Footer />
     </div>
   );
