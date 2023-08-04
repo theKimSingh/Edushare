7e8e84072fe80ad072f0de4f63408cc7f210805e
KimSinghz <ksinghz@outlook.com>
2023-08-04 11:26:08 -0700

    form

--- src/pages/find_resources.js
+++ src/pages/find_resources.js
@@ -14,51 +14,59 @@ function Find() {
           <h4>Share the gift of education.</h4>
           <h3>Find Supplies</h3>
           <div className="find-forms">
-            <label for="name">Name</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              placeholder="John Stuart"
-            />
+            <div className="left-side">
+              <label for="name">Name</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="John Stuart"
+              />
 
-            <label for="email">Email</label>
-            <input
-              type="text"
-              id="email"
-              name="email"
-              placeholder="you@example.com"
-            />
+              <label for="email">Email</label>
+              <input
+                type="text"
+                id="email"
+                name="email"
+                placeholder="you@example.com"
+              />
 
-            <label for="address">Address</label>
-            <input
-              type="text"
-              id="address"
-              name="address"
-              placeholder="123 Airo Street"
-            />
+              <label for="address">Address</label>
+              <input
+                type="text"
+                id="address"
+                name="address"
+                placeholder="123 Airo Street"
+              />
 
-            <label for="city">City</label>
-            <input type="text" id="city" name="city" placeholder="Bothell" />
+              <label for="city">City</label>
+              <input type="text" id="city" name="city" placeholder="Bothell" />
+            </div>
+            <div className="right-side">
+              <label for="state">State</label>
+              <input
+                type="text"
+                id="state"
+                name="state"
+                placeholder="Arkansas"
+              />
 
-            <label for="state">State</label>
-            <input type="text" id="state" name="state" placeholder="Arkansas" />
+              <label for="zip-code">Zip Code</label>
+              <input
+                type="text"
+                id="zip-code"
+                name="zip-code"
+                placeholder="12345"
+              />
 
-            <label for="zip-code">Zip Code</label>
-            <input
-              type="text"
-              id="zip-code"
-              name="zip-code"
-              placeholder="12345"
-            />
-
-            <label for="resource">Resource</label>
-            <select id="resource" name="resource">
-              <option value="textbook">textbook</option>
-              <option value="writing tools">Writing Tools (ex. pen)</option>
-              <option value="instruments">Instruments (ex. rulers)</option>
-            </select>
-            <input type="submit" value="Submit" id="submitx"></input>
+              <label for="resource">Resource</label>
+              <select id="resource" name="resource">
+                <option value="textbook">textbook</option>
+                <option value="writing tools">Writing Tools (ex. pen)</option>
+                <option value="instruments">Instruments (ex. rulers)</option>
+              </select>
+            </div>
+            <input type="submit" value="Submit" id="submit"></input>
           </div>
         </form>
       </div>
